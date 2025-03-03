5d51c16aa0cedb5f61e75f651a981d7ef9b50938
unknown <ahmadfaishalhidayat.03@gmail.com>
2025-03-03 11:32:06 +0700

    setup connection database mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+//connection mongodb
+mongoose.connect(process.env.db, {}).then(() => {
+  console.log("Database connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Aplikasi berjalan di port ${port}`)
-})
+  console.log(`Aplikasi berjalan di port ${port}`);
+});
